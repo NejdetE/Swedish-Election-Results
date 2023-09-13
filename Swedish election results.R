8903e921887cadfdd209290631ea222963c7d840
Nejdet Eken <118635463+NejdetE@users.noreply.github.com>
2023-09-13 13:38:03 +0200

    Update Swedish election results.R

--- Swedish election results.R
+++ Swedish election results.R
@@ -9,26 +9,26 @@ pacman::p_load(pacman, dplyr, GGally, ggplot2, ggthemes, cowplot, ggvis,
 
 #Two ways of coding the same thing slightly different results
 library(readxl)
-Elec_res <- read_excel("C:/Users/nejde/Desktop/Valresultat riksdagen.xlsx")
+Elec_res <- read_excel("xlsx-file")
 View(Elec_res)
 str(Elec_res)
 
 #1
 Elec_res %>% 
   ggplot() +
-  geom_line(aes(År, `Socialdemokraterna (S)`), colour = "red", size=1.5, alpha = 0.85) +
-  geom_line(aes(År, `Moderaterna (M)`), colour = "light blue", size=1.5, alpha = 0.85) +
-  geom_line(aes(År, `Sverigedemokraterna (SD)`), colour = "yellow", size=1.5, alpha = 0.85) +
-  geom_line(aes(År, `Miljöpartiet (MP)`), colour = "dark green", size=1.5, alpha = 0.85) +
-  geom_line(aes(År, `Centerpartiet (C)`), colour = "green", size=1.5, alpha = 0.85) +
-  geom_line(aes(År, `Kristdemokraterna (KD)`), colour = "dark blue", size=1.5, alpha = 0.85) +
-  geom_line(aes(År, `Liberalerna (L)`), colour = "blue", size=1.5, alpha = 0.85) +
-  geom_line(aes(År, `Vänsterpartiet (V)`), colour = "dark red", size=1.5, alpha = 0.85) +
+  geom_line(aes(Ã…r, `Socialdemokraterna (S)`), colour = "red", size=1.5, alpha = 0.85) +
+  geom_line(aes(Ã…r, `Moderaterna (M)`), colour = "light blue", size=1.5, alpha = 0.85) +
+  geom_line(aes(Ã…r, `Sverigedemokraterna (SD)`), colour = "yellow", size=1.5, alpha = 0.85) +
+  geom_line(aes(Ã…r, `MiljÃ¶partiet (MP)`), colour = "dark green", size=1.5, alpha = 0.85) +
+  geom_line(aes(Ã…r, `Centerpartiet (C)`), colour = "green", size=1.5, alpha = 0.85) +
+  geom_line(aes(Ã…r, `Kristdemokraterna (KD)`), colour = "dark blue", size=1.5, alpha = 0.85) +
+  geom_line(aes(Ã…r, `Liberalerna (L)`), colour = "blue", size=1.5, alpha = 0.85) +
+  geom_line(aes(Ã…r, `VÃ¤nsterpartiet (V)`), colour = "dark red", size=1.5, alpha = 0.85) +
     labs(title = "Swedish election results",
          subtitle = "Election results through time for parties currently in parliament",
          x = "Year", 
          y = "Political Parties",
-         caption = "Source: SCB Historisk statistik över valåren 1910-2014.") +
+         caption = "Source: SCB Historisk statistik Ã¶ver valÃ¥ren 1910-2014.") +
     theme(plot.title = element_text(face = "bold", size = 20), 
           plot.subtitle = element_text(face = "italic", size = 15)) +
     theme_fivethirtyeight() +
@@ -37,17 +37,17 @@ Elec_res %>%
 #2
 Elec_new <- gather(Elec_res, key = Party, value = Result,
                    c("Socialdemokraterna (S)", "Moderaterna (M)",
-                     "Sverigedemokraterna (SD)","Miljöpartiet (MP)",
+                     "Sverigedemokraterna (SD)","MiljÃ¶partiet (MP)",
                      "Centerpartiet (C)","Kristdemokraterna (KD)",
-                     "Liberalerna (L)","Vänsterpartiet (V)",))
+                     "Liberalerna (L)","VÃ¤nsterpartiet (V)",))
 Elec_new %>% 
-  ggplot(aes(År, Result, group = Party, colour = Party)) +
+  ggplot(aes(Ã…r, Result, group = Party, colour = Party)) +
   geom_line(size = 1.15) + 
        labs(title = "Swedish election results",
        subtitle = "Election results through time for parties currently in parliament",
        x = "Year", 
        y = "Voter share",
-       caption = "Source: SCB Historisk statistik över valåren 1910-2014.") +
+       caption = "Source: SCB Historisk statistik Ã¶ver valÃ¥ren 1910-2014.") +
   theme_fivethirtyeight() +
   theme(plot.title = element_text(size = 22.5),
         plot.subtitle = element_text(size = 15, face = "italic"),
